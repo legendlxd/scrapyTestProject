a98026df4ee027c35cb59797f42aa84aad506556
legendlxd <lxd.loveyellow@qq.com>
2021-08-23 14:57:30 +0800

    update

--- myfirstPj/myfirstPj/spiders/ehentai.py
+++ myfirstPj/myfirstPj/spiders/ehentai.py
@@ -11,8 +11,11 @@ class EhentaiSpider(scrapy.Spider):
     def parse(self, response):
         items = MyfirstpjItem()
         lists = response.xpath('//div[@class="glthumb"]/div/img/@src')
+        print(lists)
         titleLists = response.xpath('//div[@class="glink"]')
+        print(titleLists)
         ContentUrls = response.xpath('//td[@class="gl3c glname"]/a/@href')
+        print(ContentUrls)
         for i in lists:
             items['coverImg']=i.xpath('/@src')
             yield items
